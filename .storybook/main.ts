99e27d9f9c45487b5bfd960fd1b5dbdb1414ce27
BenukEl <benoit.rouxel@live.fr>
2025-08-26 14:29:16 +0000

    refactor: update to react 19

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,20 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from "@storybook/react-vite";
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
     "@storybook/preset-scss",
+    "@storybook/addon-docs",
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
-  core: {
-    disableTelemetry: true,
-  },
+  core: { disableTelemetry: true },
 };
+
 export default config;
